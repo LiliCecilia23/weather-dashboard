faf5a8dbc9d78523b217d14a84859524855f52bc
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-24 12:29:11 -0400

    got uv index working

--- assets/script.js
+++ assets/script.js
@@ -4,24 +4,56 @@ let searchInput = $('#searchInput'); //where the user will search for city
 let searchHistory = $('#searchHistory'); //area where local storage city searches are stored (ul element)
 let currentCity = $('#currentCity'); //where current searched city weather info is displayed
 let forecastDiv = $('#forecastDiv'); //where 5 day forecast is displayed
-
-
 let date = moment().format('L'); 
-console.log(cityName);
+const fSymbol = '\u2109'
 
-
-var apiKey = "c16c7413aa437db7ca505d50166112cf"
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
 $('#searchBtn').click(function() {
+    event.preventDefault();
     let cityName = $('#searchInput').val();
-    $('#cityName').text(cityName + "(" + date + ")");
-    console.log(cityName)
+    var apiKey = "c16c7413aa437db7ca505d50166112cf"
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    var temp;
+    
     
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
+        console.log(response);
+
+        temp = convertTemp(response.main.temp);
+        var humidity = response.main.humidity;
+        var wind = response.wind.speed;
+        var weatherEmoji = response.weather[0].icon;
+
+        $('#currentCity').addClass('border border-secondary"')
+        $('#cityName').text(cityName + " (" + date + ") " + weatherEmoji);
+
+        $('#temp').text("Temperature: " + temp + fSymbol);
+        $('#humidity').text("Humidity: " + humidity + "%");
+        $('#wind').text("Wind: " + wind + " MPH");
+        
+
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+        
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function(result) {
+            console.log(result);
+            
+            var uv = result.value;
+            $('#uv').text("UV Index: " + uv);
+        });
+
 
     })
+
+    function convertTemp (kelvin) {
+        var temp = Math.floor((kelvin - 273.15) * 1.80 + 32);
+        return temp;
+    }
 });
