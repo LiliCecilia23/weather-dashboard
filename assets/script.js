e90d7499f4492896138f387d68938da232d3e25a
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-26 15:15:24 -0400

    got local storage to work, updated API key

--- assets/script.js
+++ assets/script.js
@@ -15,15 +15,28 @@ const fSymbol = '\u2109'
 
 $('.card').css("display" , "none");
 
-$('#searchBtn').click(function() {
+function pastCities (){
+    var storedCities = $('<button>');
+    storedCities.text(window.localStorage.getItem("city"));
+    storedCities.addClass("list-group-item list-group-item-action p-3 cityBtns");
+    $('#searchHistory').prepend(storedCities);
+}
+
+pastCities();
+
+$('#searchBtn').click(function () {
     event.preventDefault();
     let cityName = $('#searchInput').val();
-    var apiKey = "c16c7413aa437db7ca505d50166112cf"
+    var apiKey = "03f0f4735c1b7aeda3d4de5aa3f8bf5a"
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
     var temp;
     var forecastTemp;
     var forecastHumidity;
     
+    
+    cityList(); 
+    storeCity();
+
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -33,7 +46,7 @@ $('#searchBtn').click(function() {
         temp = convertTemp(response.main.temp);
         var humidity = response.main.humidity;
         var wind = response.wind.speed;
-        
+
         var iconCode = response.weather[0].icon;
 
         var weatherEmoji;
@@ -176,7 +189,175 @@ $('#searchBtn').click(function() {
     function cityList (){
         var citiesSearched = $('<button>');
         citiesSearched.text(cityName);
-        $('#searchHistory').append(citiesSearched);
+        citiesSearched.addClass("list-group-item list-group-item-action p-3 cityBtns");
+        $('#searchHistory').prepend(citiesSearched);
     };
+
+    function storeCity (){
+        window.localStorage.setItem('city', cityName);
+    }
 });
 
+$('.cityBtns').click(function(){
+    event.preventDefault();
+    let pastCity = $(this).text();
+
+    console.log(pastCity)
+
+    var apiKey = "03f0f4735c1b7aeda3d4de5aa3f8bf5a"
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + pastCity + "&appid=" + apiKey;
+    var temp;
+    var forecastTemp;
+    var forecastHumidity;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+        temp = convertTemp(response.main.temp);
+        var humidity = response.main.humidity;
+        var wind = response.wind.speed;
+
+        var iconCode = response.weather[0].icon;
+
+        var weatherEmoji;
+        if (iconCode === '01d'){
+            weatherEmoji = '☀️';
+        } else if (iconCode === '01n'){
+            weatherEmoji = '🌙';
+        } else if (iconCode === '02d' || iconCode === '02n'){
+            weatherEmoji = '🌤️';
+        } else if (iconCode === '03d' || iconCode === '03n'){
+            weatherEmoji = '☁';
+        } else if (iconCode === '04d' || iconCode === '04n'){
+            weatherEmoji = '☁️';
+        } else if (iconCode === '09d' || iconCode === '09n'){
+            weatherEmoji = '🌧️';
+        } else if (iconCode === '10d' || iconCode === '10n'){
+            weatherEmoji = '🌦️';
+        } else if (iconCode === '11d' || iconCode === '11n'){
+            weatherEmoji = '⛈️';
+        } else if (iconCode === '13d' || iconCode === '12n'){
+            weatherEmoji = '❄️';
+        } else if (iconCode === '50d' || iconCode === '50n'){
+            weatherEmoji = '🌫️';
+        };
+        
+        $('#currentCity').addClass('border border-secondary"')
+        $('#cityName').text(pastCity + " (" + date + ") " + weatherEmoji);
+ 
+
+        $('#temp').text("Temperature: " + temp + fSymbol);
+        $('#humidity').text("Humidity: " + humidity + "%");
+        $('#wind').text("Wind: " + wind + " MPH");
+        
+
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+        
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function(result) {
+            var uv = result.value;
+            $('#uv').text("UV Index: ");
+            $('#uvValue').text(uv);
+            
+            if (uv < 3){
+                $('#uvValue').addClass('bg-success');
+            } else if (uv > 2 && uv < 6){
+                $('#uvValue').addClass('bg-warning');
+            }else if (uv > 5 && uv < 8){
+                $('#uvValue').addClass('highStyle');
+            } else {
+                $('#uvValue').addClass('bg-danger');
+            }
+            
+        });
+
+        $('.card').css("display" , "block");
+        $('.card').addClass('bg-primary text-white');
+        $('#forecastTitle').text("5 Day Forecast:");
+        $('#date1').text(date1);
+        $('#date2').text(date2);
+        $('#date3').text(date3);
+        $('#date4').text(date4);
+        $('#date5').text(date5);
+
+        var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + pastCity + "&appid=" + apiKey;
+
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function(result){
+            console.log(result);
+            var forecastCodes = [result.list[1].weather[0].icon, 
+            result.list[2].weather[0].icon,
+            result.list[3].weather[0].icon,
+            result.list[4].weather[0].icon,
+            result.list[5].weather[0].icon];
+
+            var emojiDivs = [$('#emoji1'),
+            $('#emoji2'),
+            $('#emoji3'),
+            $('#emoji4'),
+            $('#emoji5')];
+
+            for (var i = 0; i < forecastCodes.length; i++){
+                if (forecastCodes[i] === '01d'){
+                    emojiDivs[i].text('☀️');
+                } else if (forecastCodes[i] === '01n'){
+                    emojiDivs[i].text('🌙');
+                }else if (forecastCodes[i] === '02d' || forecastCodes[i] === '02n'){
+                    emojiDivs[i].text('🌤️');
+                } else if (forecastCodes[i] === '03d' || forecastCodes[i] === '03n'){
+                    emojiDivs[i].text('☁');
+                } else if (forecastCodes[i] === '04d' || forecastCodes[i] === '04n'){
+                    emojiDivs[i].text('☁️');
+                } else if (forecastCodes[i] === '09d' || forecastCodes[i] === '09n'){
+                    emojiDivs[i].text('🌧️');
+                } else if (forecastCodes[i] === '10d' || forecastCodes[i] === '10n'){
+                    emojiDivs[i].text('🌦️');
+                } else if (forecastCodes[i] === '11d' || forecastCodes[i] === '11n'){
+                    emojiDivs[i].text('⛈️');
+                } else if (forecastCodes[i] === '13d' || forecastCodes[i] === '13n'){
+                    emojiDivs[i].text('❄️');
+                } else if (forecastCodes[i] === '50d' || forecastCodes[i] === '50n'){
+                    emojiDivs[i].text('🌫️');
+                } 
+            }
+
+            var tempDivs = [$('#temp1'),
+            $('#temp2'),
+            $('#temp3'),
+            $('#temp4'),
+            $('#temp5')];
+
+            for (var i = 0; i < tempDivs.length; i++){
+                forecastTemp = convertTemp(result.list[i + 1].main.temp);
+                tempDivs[i].text("Temp: " + forecastTemp + fSymbol);
+            }
+
+            var humidityDivs = [$('#humidity1'),
+            $('#humidity2'),
+            $('#humidity3'),
+            $('#humidity4'),
+            $('#humidity5')]
+
+            for (var i = 0; i < humidityDivs.length; i++){
+                forecastHumidity = result.list[i + 1].main.humidity;
+                humidityDivs[i].text("Humidity: " + forecastHumidity + "%");
+            }
+
+        });
+    });
+
+    function convertTemp (kelvin) {
+        var temp = Math.floor((kelvin - 273.15) * 1.80 + 32);
+        return temp;
+    }
+
+});
